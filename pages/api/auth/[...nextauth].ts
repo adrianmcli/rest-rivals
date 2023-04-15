dd4c76f909b2dca5ed4ea358840868e489a82679
0xIvan <0xivanivan@gmail.com>
2023-04-15 22:59:46 +0900

    Fix build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,15 +23,13 @@ export const authOptions: AuthOptions = {
   callbacks: {
     // Assign access token to the session object
     async session({ session, token }) {
-      session.accessToken = token.accessToken;
-      session.user.id = token.id;
+      session.accessToken = token.accessToken as string;
       return session;
     },
     async jwt({ token, account, profile }) {
       // Persist the OAuth access_token and or the user id to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
-        token.id = profile.id;
       }
       return token;
     },
